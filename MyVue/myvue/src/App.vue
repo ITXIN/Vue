4f092cca8006c0c295fff1c47a01a7ba80e0e34b
liyaqing <ex_wljr_liyaqing@pingan.com.cn>
2018-08-06 15:32:59 +0800

    删除无用代码

--- MyVue/myvue/src/App.vue
+++ MyVue/myvue/src/App.vue
@@ -1,32 +1,15 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
+    <img src="./assets/logo.png">
+    <br>
     <!-- 这里是用来展示路由页面内容的，如果想用跳转就用<router-link to='xxx'></router-link> -->
     <!-- 是HelloWorld.vue的内容 -->
     <!-- <router-view/>   -->
-
-    <!-- <h1>App.vue</h1>
-    <p>{{message}}</p>
-    <input type="text" v-model="message" />
-    <ul>
-      <li v-for="person in people" style="margin:10px">
-        name:{{ person.name }},age:{{person.age}},sex:{{person.sex}}
-      </li>
-    </ul> -->
-      <!-- <h1>商品展示</h1> 
-      <div v-for="(good, index) in dataArr" :key="index" style="margin:10px,">
-        <div style="float:left">
-          <img :src="good.img" width="100px" height="100px">
-          <p style="background:white;margin:10px">价格:{{ good.price }},商品名:{{good.title}},编号:000{{index}}</p>
-      </div>
-     </div> -->
-     
 
     <GoodsList v-for="(good, index) in dataArr" :key="index" v-bind:good="good" >
     </GoodsList> 
 
     <router-link to="/HelloWorld"> HelloWord</router-link>
-    <!-- <router-link to="/GoodsList"> GoodsList</router-link> -->
     <router-view></router-view>
 
   </div>
@@ -38,6 +21,7 @@ import GoodsList from '@/components/GoodsList'
 export default {
   name: 'App',
   created() {
+    //获取数据
     this.$http.get('/api/goods').then((data) => {
       if(data.body.code == 0){
         this.dataArr = data.body.data;
@@ -48,34 +32,17 @@ export default {
   components:{
       GoodsList
   },
-  //  methods: {
-  //   goBack () {
-  //       window.history.length > 1
-  //           ? this.$router.go(-1)
-  //           : this.$router.push('/')
-  //       }
-  //   },
+   methods: {
+    goBack () {
+        window.history.length > 1
+            ? this.$router.go(-1)
+            : this.$router.push('/')
+        }
+    },
   data(){
     return{
       message:"app.message",
       dataArr:[],
-      people: [{
-                    name: 'Jack',
-                    age: 30,
-                    sex: 'Male'
-                }, {
-                    name: 'Bill',
-                    age: 26,
-                    sex: 'Male'
-                }, {
-                    name: 'Tracy',
-                    age: 22,
-                    sex: 'Female'
-                }, {
-                    name: 'Chris',
-                    age: 36,
-                    sex: 'Male'
-                }],
      
     }
   }
