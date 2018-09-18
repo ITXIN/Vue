8c270cb9c14ae4a1608258ec1d5b94da52868a57
鑫龙魂 <liyaqing_it@126.com>
2018-09-18 17:18:40 +0800

    添加alerttip

--- MyVue/myvue/src/App.vue
+++ MyVue/myvue/src/App.vue
@@ -7,51 +7,57 @@
     <!-- <router-view/>   -->
 
     <GoodsList v-for="(good, index) in dataArr" :key="index" v-bind:good="good" >
-    </GoodsList> 
+    </GoodsList>  -->
 
     <router-link to="/HelloWorld"> HelloWord</router-link>
     <router-view></router-view>
 
+ 
+<alert-tip :alertText="alertText"> </alert-tip>  
   </div>
   
 </template>
 
 <script>
-import GoodsList from '@/components/GoodsList'
+import GoodsList from "@/components/GoodsList";
+
+import alertTip from "@/components/common/alertTip";
+import GoodsDetail from "@/components/GoodsDetail";
+
 export default {
-  name: 'App',
+  name: "App",
   created() {
     //获取数据
-    this.$http.get('/api/goods').then((data) => {
-      if(data.body.code == 0){
+    this.$http.get("/api/goods").then(data => {
+      if (data.body.code == 0) {
         this.dataArr = data.body.data;
-        console.log(this.dataArr)
+        console.log(this.dataArr);
       }
-    })
+    });
   },
-  components:{
-      GoodsList
+  components: {
+    GoodsList,
+    alertTip
   },
-   methods: {
-    goBack () {
-        window.history.length > 1
-            ? this.$router.go(-1)
-            : this.$router.push('/')
-        }
-    },
-  data(){
-    return{
-      message:"app.message",
-      dataArr:[],
-     
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
     }
+  },
+  data() {
+    return {
+      message: "app.message",
+      dataArr: [],
+      showAlert: true,
+      alertText: "kdkfk"
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ MyVue/myvue/src/components/common/alertTip.vue
@@ -0,0 +1,110 @@
+ <template>
+    <div class="alet_container">
+	    <section class="tip_text_container">
+            <div class="tip_icon">
+                <span></span>
+                <span></span>
+            </div>
+            <p class="tip_text">{{alertText}}</p>
+            <div class="confrim" @click="closeTip">确认</div>
+        </section>
+      
+    </div>
+
+    <!-- <div>{{alertText}}</div> -->
+</template>
+
+<script>
+    export default {
+        name: 'alertTip',
+    	data(){
+            return{
+                positionY: 0,
+                timer: null,
+            }
+        },
+        mounted(){
+      
+        },
+        props: ['alertText'],
+        methods: {
+            closeTip(){
+                this.$emit('closeTip')
+            }
+        }
+    }
+</script>
+
+<style  scoped>
+
+	@keyframes tipMove{
+       0%   { transform: scale(1) }
+       35%  { transform: scale(.8) }
+       70%  { transform: scale(1.1) }
+       100% { transform: scale(1) }
+    }
+    .alet_container{
+    	position: fixed;
+    	top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 200;
+    }
+    .tip_text_container{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-top: -6rem;
+        margin-left: -6rem;
+        width: 12rem;
+        animation: tipMove .4s ;
+        background-color: rgba(25,255,255,1);
+        border: 1px;
+        padding-top: .6rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        border: 1px;
+        border-radius: 0.25rem;
+        
+    }
+    .tip_icon{
+            border: 0.15rem solid #f8cb86;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            /* span:nth-of-type(1){
+                background-color: #f8cb86;
+            };
+            span:nth-of-type(2){ */
+                
+                border: 1px;
+                border-radius: 50%;
+                margin-top: .2rem;
+                background-color: #f8cb86;
+            /* }; */
+        }
+        .tip_text{
+           
+            line-height: .9rem;
+            text-align: center;
+            margin-top: .8rem;
+            padding: 0 .4rem;
+        }
+        .confrim{
+           
+            font-weight: bold;
+            margin-top: .8rem;
+            background-color: #4cd964;
+            width: 100%;
+            text-align: center;
+            line-height: 1.8rem;
+            border: 1px;
+            border-bottom-left-radius: 0.25rem;
+            border-bottom-right-radius: 0.25rem;
+        }
+</style>
