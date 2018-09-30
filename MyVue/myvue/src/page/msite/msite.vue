a8469fed21dcfdcad607917eba9c6a5877cc3ae4
鑫龙魂 <liyaqing_it@126.com>
2018-09-30 15:31:06 +0800

    添加一些商铺列表相关页面

--- MyVue/myvue/src/page/msite/msite.vue
+++ MyVue/myvue/src/page/msite/msite.vue
@@ -13,26 +13,30 @@
         </head-top>
 
         <nav class="msite_nav">
-            <!-- <div class="swiper-container" v-if="foodTypes.length">
+            <div class="swiper-container" v-if="foodTypes.length">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide food_types_container" v-for="(item,index) in foodTypes" :key="index">
                         <router-link :to="{path:'/food', query:{geohash,title:foodItem.title,restaurant_category_id:getCategoryId(foodItem.link)}}" v-for="foodItem in item" :key="foodItem.id" class="link_to_food">
                             <figure>
-                                <img src="imgBaseUrl + foodItem.image_url" alt="">
+                                <img :src="imgBaseUrl + foodItem.image_url" alt="">
                                 <figcaption>{{foodItem.title}}</figcaption>
                             </figure>
                         </router-link>
                     </div>
-                    <div class="swiper-pagination"></div>
                 </div>
-            </div> -->
-            <!-- <img src="../../images/fl.svg" class="fl_back animation_opactiy" v-else alt=""> -->
+                <div class="swiper-pagination"></div>
+            </div>
+            <img src="../../images/fl.svg" class="fl_back animation_opactiy" v-else >
         </nav>
 
         <div class="shop_list_container">
-            <header >
-
+            <header class="shop_header" >
+                <svg class="shop_icon">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shop"></use>
+                </svg>
+                <span class="shop_header_title">附近商家</span>
             </header>
+            <shop-list v-if="hasGetData" :geohash="geohash"></shop-list>
         </div>
 
         <foot-guide></foot-guide>
@@ -45,6 +49,9 @@ import { mapMutations } from "vuex";
 import headTop from "../../components/header/head";
 import footGuide from "../../components/footer/footGuide";
 import { cityGuess, msiteAddress, msiteFoodTypes } from "../../service/getData";
+import '../../plugins/swiper.min.js';
+import '../../style/swiper.min.css';
+import shopList from '../../components/common/shoplist';
 
 export default {
   data() {
@@ -66,22 +73,129 @@ export default {
       this.geohash = this.$route.query.geohash;
       console.log("geohash", this.geohash);
     }
+
     this.SAVE_GEOHASH(this.geohash);
     let res = await msiteAddress(this.geohash);
+
     console.log("当前地址", res);
     this.msiteTitle = res.name;
+    this.RECORD_ADDRESS(res);
+    this.hasGetData = true;
   },
-  mounted() {},
+  mounted() {
+      //获取导航食品类型列表
+      msiteFoodTypes(this.geohash).then(res=>{
+          let resLength = res.length;
+          let resArr = [...res];//返回一个新数组
+          
+          let foodArr = [];
+          for (let i = 0,j = 0; i < resLength; i += 8,j++) {
+              foodArr[j] = resArr.splice(0,8); 
+          }
+          this.foodTypes = foodArr;
+      }).then(()=>{
+          new Swiper('.swiper-container', {
+		        pagination: '.swiper-pagination',
+		        loop: true
+		    });
+      })
+  },
   components: {
     headTop,
-    footGuide
+    footGuide,
+    shopList
   },
   computed: {},
   methods: {
-    ...mapMutations(["RECORD_ADDRESS", "SAVE_GEOHASH"])
+    ...mapMutations([
+        "RECORD_ADDRESS", 
+        "SAVE_GEOHASH"
+        ]),
+        // 解码url地址，求去restaurant_category_id值
+    getCategoryId(url){
+        let urlData = decodeURIComponent(url.split('=')[1].replace('&target_name',''));
+        if (/restaurant_category_id/gi.test(urlData)) {
+            return JSON.parse(urlData).restaurant_category_id.id;
+        }else{
+            return '';
+        }
+    },
   }
 };
 </script>
 
-<style>
+
+<style lang="scss" scoped>
+    @import 'src/style/mixin';
+	.link_search{
+		left: .8rem;
+		@include wh(.9rem, .9rem);
+		@include ct;
+	}
+	.msite_title{
+		@include center;
+        width: 50%;
+        color: #fff;
+        text-align: center;
+        margin-left: -0.5rem;
+        .title_text{
+            @include sc(0.8rem, #fff);
+            text-align: center;
+            display: block;
+        }
+	}
+	.msite_nav{
+		padding-top: 2.1rem;
+		background-color: #fff;
+		border-bottom: 0.025rem solid $bc;
+		height: 10.6rem;
+		.swiper-container{
+			@include wh(100%, auto);
+			padding-bottom: 0.6rem;
+			.swiper-pagination{
+				bottom: 0.2rem;
+			}
+		}
+		.fl_back{
+			@include wh(100%, 100%);
+		}
+	}
+	.food_types_container{
+		display:flex;
+		flex-wrap: wrap;
+		.link_to_food{
+			width: 25%;
+			padding: 0.3rem 0rem;
+			@include fj(center);
+			figure{
+				img{
+					margin-bottom: 0.3rem;
+					@include wh(1.8rem, 1.8rem);
+				}
+				figcaption{
+					text-align: center;
+					@include sc(0.55rem, #666);
+				}
+			}
+		}
+	}
+	.shop_list_container{
+		margin-top: .4rem;
+		border-top: 0.025rem solid $bc;
+		background-color: #fff;
+		.shop_header{
+			.shop_icon{
+				fill: #999;
+				margin-left: 0.6rem;
+				vertical-align: middle;
+				@include wh(0.6rem, 0.6rem);
+			}
+			.shop_header_title{
+				color: #999;
+				@include font(0.55rem, 1.6rem);
+			}
+		}
+	}
+
 </style>
+
